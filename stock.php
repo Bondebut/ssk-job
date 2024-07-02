faff7c8b702bcb0eece900a1a097bab60ec57ccb
Bondebut <catran109@gmail.com>
2024-07-02 09:58:46 +0700

    this my job

--- stock.php
+++ stock.php
@@ -54,30 +54,7 @@
 </head>
 <body>
      <!-- Navbar -->
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">
-                <img src="img/clipart1764470.png" alt="Logo" width="30" height="30">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Office</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="gallery.html">Gallery</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+     <?php include_once('nav.php'); ?>
 
     <!--bodycontent-->
     <div class="container">
